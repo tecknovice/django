5e1680aa4a7c10984e12abd35036cb2ac6c06d08
tecknovice <tecknovice@gmail.com>
2023-10-03 10:25:42 +0700

    customizing admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import Question, Choice
 
+
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ["question_text"]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,7 +1,9 @@
 import datetime
 
+from django.contrib import admin
 from django.db import models
-from django.utils import  timezone
+from django.utils import timezone
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -10,9 +12,14 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
-        return now >= self.pub_date  >= now - datetime.timedelta(days=1)
+        return now >= self.pub_date >= now - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
